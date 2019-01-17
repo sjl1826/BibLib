21eea19d57bca5961b72e77aed93347a57226e34
= <heysamlee@gmail.com>
2019-01-17 01:08:43 -0800

    migrated the created models into database

--- BibLib/settings.py
+++ BibLib/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bibs.apps.BibsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
