b5a8769f8553f488c41ffc522c2bbdcfac096265
= <heysamlee@gmail.com>
2019-01-17 10:00:22 -0800

    restructured models and added admin access site

--- bibs/models.py
+++ bibs/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-class Chapter(models.Model):
-    chapter = models.CharField(max_length=10)
-
 class Book(models.Model):
     name = models.CharField(max_length=30)
-    chapters = models.ManyToManyField(Chapter)
+    numChapters = models.IntegerField(default=0)
