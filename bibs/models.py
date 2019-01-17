c868b1c1935daf167e0c83995c2b6e946eead5f9
= <heysamlee@gmail.com>
2019-01-17 01:19:02 -0800

    restructured models

--- bibs/models.py
+++ bibs/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
-class Passage(models.Model):
-    book = models.CharField(max_length=30)
+class Chapter(models.Model):
     chapter = models.CharField(max_length=10)
+
+class Book(models.Model):
+    name = models.CharField(max_length=30)
+    chapters = models.ManyToManyField(Chapter)
