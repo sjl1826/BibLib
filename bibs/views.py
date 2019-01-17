5787aa5a8b6372c1b82413523e16ae389b7a4bec
= <heysamlee@gmail.com>
2019-01-17 10:25:01 -0800

    added get passage function, added path to function

--- bibs/views.py
+++ bibs/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.http import JsonResponse
+from django.views import generic
+from .models import Book
+import json
+import random
 
 def index(request):
     return HttpResponse("Hello, you are at the bibs index.")
+
+def randomBiblePassage(request):
+    books = Book.objects.all()
+    bookNum = random.randint(0, 65)
+    book = books[bookNum].name
+    maxChapters = random.randint(1, books[bookNum].numChapters)
+    finalPassage = book + " " + str(maxChapters)
+    dict = { "passage": finalPassage }
+    return JsonResponse(dict)
